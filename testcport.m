66b2cb7af9358fcdf3b5eaaad2c9da8c021dafa1
gbenga <gbadipe@gmail.com>
2013-01-11 09:54:31 -0600

    Adding files intended to be ported to C using Matlab

---
+++ testcport.m
@@ -0,0 +1,13 @@
+%#codegen
+function [y] = testcport()
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+y = 'Hello world';
+j = 0;
+for i=1:6  
+    j = j + 1;
+end
+
+
+end
