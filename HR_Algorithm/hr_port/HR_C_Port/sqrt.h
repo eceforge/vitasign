5e5b9c0b88696b69235aa7c6516310686f2c7f17
gbenga <gbadipe@gmail.com>
2013-03-26 18:57:12 -0500

    Added the microcontroller project

---
+++ HR_Algorithm/hr_port/HR_C_Port/sqrt.h
@@ -0,0 +1,32 @@
+/*
+ * sqrt.h
+ *
+ * Code generation for function 'sqrt'
+ *
+ * C source code generated on: Mon Mar 11 20:10:04 2013
+ *
+ */
+
+#ifndef __SQRT_H__
+#define __SQRT_H__
+/* Include files */
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rtwtypes.h"
+#include "heart_rate_official_cport_types.h"
+
+/* Type Definitions */
+
+/* Named Constants */
+
+/* Variable Declarations */
+
+/* Variable Definitions */
+
+/* Function Declarations */
+extern int16_T b_sqrt(int32_T x);
+#endif
+/* End of code generation (sqrt.h) */
